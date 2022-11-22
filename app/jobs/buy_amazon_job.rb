bc2645ce19240c960bdf5db3561d7869445eb169
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-11-22 15:33:03 +0900

    chrome接続完了

---
+++ app/jobs/buy_amazon_job.rb
@@ -0,0 +1,26 @@
+
+
+
+class BuyAmazonJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+
+    @wait_time = 10 
+    @timeout = 180
+
+    Selenium::WebDriver.logger.output = File.join("./", "selenium.log")
+    Selenium::WebDriver.logger.level = :warn
+
+    driver = Selenium::WebDriver.for :remote, url: 'http://chrome:4444/wd/hub', capabilities: [:chrome]
+
+    driver.manage.timeouts.implicit_wait = @timeout
+    wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
+
+    driver.get('https://www.yahoo.co.jp/')
+
+    sleep 2
+
+    driver.quit
+  end
+end
