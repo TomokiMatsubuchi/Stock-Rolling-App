8cef32157d319a51c2746fd69cff280d98c05164
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-12-16 18:55:19 +0900

    不要な空白の削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe "バリデーションのテスト" do
     context "ユーザ情報が全てある場合" do
       it "ユーザが登録できる" do
-        user = FactoryBot.create(:user_1)
+        user = User.find_by(email: "sample_taro@example.com") || FactoryBot.create(:user_1)
         expect(user).to be_valid
       end
     end
