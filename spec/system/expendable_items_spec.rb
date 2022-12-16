9b30842ce8c6bceaabc2749abdc2cdf21990a29f
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-12-16 11:39:56 +0900

    Footer固定 + アクセス制限テスト追加

--- spec/system/expendable_items_spec.rb
+++ spec/system/expendable_items_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "消耗品情報管理機能", type: :system do
+  
   describe '消耗品情報登録' do
     before do
       Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
@@ -33,19 +34,33 @@ RSpec.describe "消耗品情報管理機能", type: :system do
     context '消費開始日の更新をする場合' do
       it '一覧にて本日を消費開始日に再設定しましたと表示が出る' do
         page.all('.show-item')[0].click
-        page.accept_confirm do
-          click_on '本日を消費開始日に再設定する'
-        end
+        click_link '本日を消費開始日に再設定する'
         expect(page).to have_content '本日を消費開始日に再設定しました!'
       end
     end
 
     context '消耗品情報を削除する場合' do
       it '消耗品情報を削除できる' do
-        page.accept_confirm do
-          page.all('.destroy-item')[0].click
-        end
+        page.all('.destroy-item')[0].click
         expect(page).to have_content '消耗品情報を削除しました!'
+      end
+    end
+  end
+
+  describe 'アクセス制限に関するテスト' do
+    
+    before do
+      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:line]
+      visit new_user_session_path
+      click_on  'line_login'
+    end
+    context '他者の消耗品情報にアクセスしようとした場合' do
+      it 'アクセスに失敗する' do
+        user = User.find_by(email: "sample_taro@example.com") || FactoryBot.create(:user_1)
+        other_user_item = FactoryBot.create(:item_1, user: user)
+        visit expendable_item_path(other_user_item.id)
+        expect(page).to have_content 'アクセス権限がありません'
       end
     end
   end
